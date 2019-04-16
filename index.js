a712828c1e3709fb35f203b1a3748e0b0e2e78f1
Chet Corcos <ccorcos@gmail.com>
2019-04-16 13:40:56 -0700

    First

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+
+app.use(function(req, res, next) {
+	var data = ""
+	req.setEncoding("utf8")
+	req.on("data", function(chunk) {
+		data += chunk
+	})
+
+	req.on("end", function() {
+		req.body = data
+		next()
+	})
+})
+
+app.post("*", (req, res) => {
+	console.log(req.body)
+	res.status(200).send()
+})
+
+app.listen(8000, () => {
+	console.log("Listening at http://localhost:8000")
+})
